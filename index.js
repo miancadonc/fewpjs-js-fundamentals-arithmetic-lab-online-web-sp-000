87e8264aa79a31916a7ca1ad618f81710c9c9e1b
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-25 16:28:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+
+    let num1 = 31
+    let num2 = 2
+    const multiply = num1 * num2
+
+    const random = Math.ceil(Math.random * 10)
+
+    const mod = 25 % 7
+
+    const max = Math.max(1, 2, 3, 20)
